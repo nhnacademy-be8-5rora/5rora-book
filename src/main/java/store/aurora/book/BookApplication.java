631e9992ccbb2d6a27633b2a4073ae2f8898547b
bangjun-yeop <wnsduq1225@gmail.com>
2024-12-10 12:33:07 +0900

    create book

---
+++ src/main/java/store/aurora/book/BookApplication.java
@@ -0,0 +1,13 @@
+package store.aurora.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookApplication.class, args);
+	}
+
+}
